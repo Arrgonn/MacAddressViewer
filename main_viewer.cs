fc96b8d6e09090e4cd1e88b4c185a86e7ba98732
Arrgonn <46163471+Arrgonn@users.noreply.github.com>
2024-02-24 19:22:35 +0000

    MacAdresse_Viewer

---
+++ main_viewer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        NetworkInterface nic = NetworkInterface.GetAllNetworkInterfaces()[0];
+        PhysicalAddress macAddress = nic.GetPhysicalAddress();
+        string macString = string.Join("-", macAddress.GetAddressBytes().Select(b => b.ToString("X2")));
+        Console.WriteLine("L'adresse MAC de {0} est {1}", nic.Description, macString);
+    }
+}
